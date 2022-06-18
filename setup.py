ed416309bb09751764ecaadd2ecb0f370380435d
Avnish327030 <anishyadav7045075175@gmail.com>
2022-06-18 12:18:37 +0530

    folderstructure completed

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
     """
+   
     Description: This function is going to return list of requirement 
     mention in requirements.txt file
 
