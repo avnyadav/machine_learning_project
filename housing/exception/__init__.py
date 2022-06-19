82553bacf548cb79993169f9cec691d3f51b340e
Avnish327030 <anishyadav7045075175@gmail.com>
2022-06-19 11:46:36 +0530

    added ml pipeline image:

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,7 +19,7 @@ class HousingException(Exception):
         _,_ ,exec_tb = error_detail.exc_info()
         line_number = exec_tb.tb_frame.f_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scrip: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
         return error_message
 
     def __str__(self):
